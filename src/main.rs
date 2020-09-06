8f32f3aa62a68477b8a5eb99b9eb5395f4eaf5ec
Sergey <sergey.bibikov.92@mail.ru>
2020-09-06 18:04:24 +0300

    minor changes

--- src/main.rs
+++ src/main.rs
@@ -32,8 +32,9 @@ fn main() {
             single_req(&inp)
         },
         "c"=>{
+            let req_num:usize = args[3].parse().unwrap();
             inp = init().unwrap();
-            bench(&inp);
+            bench(&inp,&req_num);
         },
         _ => {print!("No mode specified");std::process::exit(1)}
     }
@@ -71,21 +72,21 @@ fn single_req(req_d: &Request){
     }
 }
 
-fn bench (req_d: &Request){
+fn bench (req_d: &Request, req_num: &usize){
     if req_d.protocol == "http".to_string() && req_d.method=="GET"{
-        http_get(&req_d.path, &req_d.domain, &req_d.port, &req_d.headers, &req_d.max_reqs_per_conn);
+        http_get(&req_d.path, &req_d.domain, &req_d.port, &req_d.headers, &req_d.max_reqs_per_conn, req_num);
     }
     else if req_d.protocol == "http".to_string() && req_d.method=="POST"{
         let temp_req_body = fs::read(&req_d.path_to_body).unwrap();
         let req_body = String::from_utf8(temp_req_body).unwrap();
-        http_post(&req_d.path, &req_d.domain, &req_d.port, &req_body, &req_d.headers, &req_d.max_reqs_per_conn);
+        http_post(&req_d.path, &req_d.domain, &req_d.port, &req_body, &req_d.headers, &req_d.max_reqs_per_conn, req_num);
     }
     else if req_d.protocol == "https".to_string() && req_d.method=="GET"{
-        https_get(&req_d.path, &req_d.domain, &req_d.port, &req_d.headers, &req_d.max_reqs_per_conn);
+        https_get(&req_d.path, &req_d.domain, &req_d.port, &req_d.headers, &req_d.max_reqs_per_conn, req_num);
     }
     else {
         let temp_req_body = fs::read(&req_d.path_to_body).unwrap();
         let req_body = String::from_utf8(temp_req_body).unwrap();
-        https_post(&req_d.path, &req_d.domain, &req_d.port, &req_body, &req_d.headers, &req_d.max_reqs_per_conn);
+        https_post(&req_d.path, &req_d.domain, &req_d.port, &req_body, &req_d.headers, &req_d.max_reqs_per_conn, req_num);
     }
 }
